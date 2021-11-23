94369a5ef6dbb865af04ce0fdb8509285dabdefb
Achille Monod <achille.monod&@gmail.com>
2021-11-23 15:11:29 +0100

    views classique

--- mon-rdv-boot/src/main/java/sopra/formation/model/View.java
+++ mon-rdv-boot/src/main/java/sopra/formation/model/View.java
@@ -3,9 +3,31 @@ package sopra.formation.model;
 
 public class View {
 
-	
-	
 	public static class ViewCommon {
 	}
 
+	public static class ViewConsultation {
+	}
+	
+	public static class ViewLieu {
+	}
+	
+	public static class ViewCreneau {
+	}
+	
+	public static class ViewMotif {
+	}
+	
+	public static class ViewPatient {
+	}
+	
+	public static class ViewPraticien {
+	}
+	
+	public static class ViewSpecialite {
+	}
+	
+	public static class ViewUtilisateur {
+	}
+	
 }
