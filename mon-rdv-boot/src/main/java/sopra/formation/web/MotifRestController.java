f4b2528f27ff7164919290a3412bd8b87b13393b
Vincent <vincent.dehouve@supbioteh.fr>
2021-11-23 15:18:51 +0100

    motif

--- mon-rdv-boot/src/main/java/sopra/formation/web/MotifRestController.java
+++ mon-rdv-boot/src/main/java/sopra/formation/web/MotifRestController.java
@@ -1,5 +1,88 @@
 package sopra.formation.web;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import sopra.formation.model.Motif;
+import sopra.formation.repository.IMotifRepository;
+
+@RestController
+@RequestMapping("/motif")
+@CrossOrigin("*")
 public class MotifRestController {
+
+	@Autowired
+	private IMotifRepository motifRepo;
+
+	@GetMapping("")
+	@JsonView(View.ViewMotif.class)
+	public List<Motif> findAll() {
+		List<Motif> motifs = motifRepo.findAll();
+
+		return motifs;
+	}
+
+	@GetMapping("{id}")
+	@JsonView(View.ViewMotifDetail.class)
+	public Motif find(@PathVariable Long id) {
+		Optional<Motif> optMotif = motifRepo.findById(id);
+
+		if (optMotif.isPresent()) {
+			return optMotif.get();
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Motif non trouvé");
+		}
+	}
+
+	@PostMapping("")
+	@JsonView(View.ViewMotif.class)
+	public Motif create(@RequestBody Motif motif, BindingResult result) {
+		if (result.hasErrors()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Motif invalide !");
+		}
+
+		motif = motifRepo.save(motif);
+
+		return motif;
+	}
+
+	@PutMapping("/{id}")
+	@JsonView(View.ViewMotif.class)
+	public Motif update(@PathVariable Long id, @RequestBody Motif motif) {
+		if (!motifRepo.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Motif non trouvé");
+		}
+
+		motif = motifRepo.save(motif);
+
+		return motif;
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		if (!motifRepo.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Motif non trouvé");
+		}
+
+		motifRepo.deleteById(id);
+	}
+
+}
 
 }
