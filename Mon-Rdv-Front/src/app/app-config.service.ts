3a5a11842930e3e1acb29393e8a717438738db9c
Vincent <vincent.dehouve@supbioteh.fr>
2021-12-01 12:04:43 +0100

    config civilite

--- Mon-Rdv-Front/src/app/app-config.service.ts
+++ Mon-Rdv-Front/src/app/app-config.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,8 @@ export class AppConfigService {
   backEndUrl: string = "http://localhost:8080/";
 
   constructor(private http : HttpClient) { }
-
+  findAllCivilites(): Observable<Array<string>> {
+    return this.http.get<Array<string>>(this.backEndUrl + "civilites");
+  }
   
 }
