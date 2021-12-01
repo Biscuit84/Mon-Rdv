cc0386c7dcef6fb821397cfb014e5aa599f70aa9
Biscay <hugo.bcpst@gmail.com>
2021-12-01 13:50:01 +0100

    rajout

--- Mon-Rdv-Front/src/app/app.component.ts
+++ Mon-Rdv-Front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Utilisateur } from './module';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mon-Rdv-Front';
+  utilisateurConnect:Utilisateur;
 }
